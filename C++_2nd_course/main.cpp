3bfb1483a9997ac7a746efc291010d01a26cbd3e
Alemek1075 <al.bazylevich@gmail.com>
2025-10-03 21:00:54 +0300

    main begining

--- C++_2nd_course/main.cpp
+++ C++_2nd_course/main.cpp
@@ -9,3 +9,14 @@ public:
     Point() {};
     Point(double _x, double _y) : x(_x), y(_y) {};
 };
+
+
+int main() {
+    std::ifstream fin("input.txt");
+    std::ofstream fout("output.txt");
+
+    if (!fin.is_open() || !fout.is_open()) {
+        std::cerr << "Error while trying to open input file.\n";
+        return 1;
+    }
+}
