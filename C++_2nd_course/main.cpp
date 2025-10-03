f86c37b551df8322fc46f2dca9aaacb61f292ceb
Alemek1075 <al.bazylevich@gmail.com>
2025-10-03 22:00:11 +0300

    ufdsgihjdhf

--- C++_2nd_course/main.cpp
+++ C++_2nd_course/main.cpp
@@ -3,6 +3,15 @@
 #include <vector>
 #include <random>
 
+
+struct Point {
+    double x, y;
+public:
+    Point() {};
+    Point(double _x, double _y) : x(_x), y(_y) {};
+};
+
+
 //123
 
 struct Matrix {
