e7a7c5be0c89af06c85f524483ef894f65b44bb3
Alemek1075 <al.bazylevich@gmail.com>
2025-10-03 20:57:34 +0300

    Header

---
+++ C++_2nd_course/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <random>
+
+struct Point {
+    double x, y;
+public:
+    Point() {};
+    Point(double _x, double _y) : x(_x), y(_y) {};
+};
